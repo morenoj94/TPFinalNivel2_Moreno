0f8e1e6ff19e691b5c66e995140162267152bc9c
Jesus Moreno <ing.jmoreno94@gmail.com>
2023-11-01 22:32:45 -0500

    Se crean las clases necesarias dentro del proyecto Domain

---
+++ Domain/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    internal class Article
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public Brand articleBrand { get; set; }
+        public Category articleCategory { get; set; }
+        public string urlImagen { get; set; }
+        public decimal Price { get; set; }
+    }
+}
